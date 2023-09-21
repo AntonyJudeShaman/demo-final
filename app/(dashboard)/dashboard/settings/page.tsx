82f13aa43b6851181b4389d43e2669ab47eef851
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2023-09-21 22:25:16 +0530

    gg

--- app/(dashboard)/dashboard/settings/page.tsx
+++ app/(dashboard)/dashboard/settings/page.tsx
@@ -25,7 +25,15 @@ export default async function SettingsPage() {
         text="Manage account and website settings."
       />
       <div className="grid gap-10">
-        <UserNameForm user={{ id: user.id, name: user.name || "" , email: user.email || ""}} />
+      <UserNameForm
+  user={{
+    id: user.id,
+    name: user.name || "",
+    email: user.email || "", // Use optional chaining and provide a default value
+    image: user?.image || "", // Use optional chaining and provide a default value
+  }}
+/>
+
       </div>
     </DashboardShell>
   )
